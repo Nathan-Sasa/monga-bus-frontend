020f91c7e6e21c26de0e165e188e2e4f631a5a93
Nathan-Sasa <jonathanbikuta05@gmail.com>
2026-09-09 21:41:59 +0100

    change: environment url momentannement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
-const url = 'http://localhost:8080'
+// const url = 'http://localhost:8080'
+const url = 'assets/mock/apis'
 export const environment = {
     apiUrls: {
         vols: `${url}/currentVols.json`
